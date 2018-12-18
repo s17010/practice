d03ef04b9d585748cb8d588ec2eba0805a725876
s17010 <s17010@std.it-college.ac.jp>
2018-12-18 09:20:48 +0900

    じゃんけん

--- Jan/Jan.java
+++ Jan/Jan.java
@@ -1,6 +1,5 @@
 import java.util.Random;
 import java.util.Scanner;
-import java.util.InputMismatchException;
 class Jan {
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
@@ -8,19 +7,22 @@ class Jan {
 		boolean check = true;
 		while(check) {
 			int pan = rand.nextInt(3);
-			System.out.println("数値を入力(0:グー,1:チョキ,2:パー):");
-			int pon = scan.nextInt();
+			System.out.print("数値を入力(0:グー, 1:チョキ, 2:パー):");
+			String pon = scan.nextLine();
 			switch(pon) {
-				case 0:
+				case "0":
 					switch(pan) {
 						case 0:
+							System.out.println("computer is グー");
 							System.out.println("あいこ。もう一度");
 							break;
 						case 1:
+							System.out.println("computer is チョキ");
 							System.out.println("player win!!");
 							check = false;
 							break;
 						case 2:
+							System.out.println("computer is パー");
 							System.out.println("you lose");
 							check = false;
 							break;
@@ -28,16 +30,19 @@ class Jan {
 							System.out.println("error");
 					}
 				break;
-				case 1:
+				case "1":
 					switch(pan) {
 						case 0:
+							System.out.println("computer is グー");
 							System.out.println("you lose");
 							check = false;
 							break;
 						case 1:
+							System.out.println("computer is チョキ");
 							System.out.println("引き分け。もう一度");
 							break;
 						case 2:
+							System.out.println("computer is パー");
 							System.out.println("player win");
 							check = false;
 							break;
@@ -45,17 +50,20 @@ class Jan {
 							System.out.println("error");
 					}
 				break;
-				case 2:
+				case "2":
 					switch(pan) {
 						case 0:
+							System.out.println("computer is グー");
 							System.out.println("player win!!");
 							check = false;
 							break;
 						case 1:
+							System.out.println("computer is チョキ");
 							System.out.println("you lose");
 							check = false;
 							break;
 						case 2:
+							System.out.println("computer is パー");
 							System.out.println("引き分け。もう一度");
 							break;
 						default:
